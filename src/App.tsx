afd95d20d879c377ae86452a5a932a571fd0359c
siangi <siangi@gmx.ch>
2023-05-07 17:32:55 +0200

    forgot to commit before, put qucik controls in is own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
-import { FaCog } from 'react-icons/fa'
+import React, { useContext, useState } from 'react';
 import './App.css';
 import MasonryGallery from './Components/Gallery/MasonryGallery';
-import ImageProvider from './Contexts/imageContext';
+import ImageProvider, { ImageContext } from './Contexts/imageContext';
 import Menu from './Components/Menu/Menu';
-import IconKnob from './Components/Controls/IconKnob';
+import QuickControls from './Components/Menu/QuickControls';
+
 
 
 
 function App() {
   const [isMenuVisible, setIsMenuVisible] = useState<Boolean>(false)
+  
   return (
     <div className="App">
       <ImageProvider>
         {isMenuVisible ? <Menu closeAction={(event) => setIsMenuVisible(false)}></Menu>
-          : <IconKnob icon={<FaCog />} onClick={(event: React.MouseEvent) => setIsMenuVisible(true)}></IconKnob>}
+          : (<QuickControls toggleMenu={setIsMenuVisible}></QuickControls>)}
         <MasonryGallery></MasonryGallery>
       </ImageProvider>
     </div>
